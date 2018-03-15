6028545d9fd4113f81b8c44f538be824c1f4e8f3
Jake <jmleavell90@gmail.com>
2018-03-15 16:48:01 -0700

    working firebase auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Chat from '../components/Chat'
 import { Socket } from 'socket.io-client'
 import Login from '../components/Login';
+import API from '../../config.js';
+import firebase from 'firebase';
 
 
 class App extends Component {
@@ -18,10 +20,12 @@ class App extends Component {
 	}
 	//LOGIN PAGE COMPONENTS
 	createUserHandler(e) {
-		//do some stuff
+		const errHandler = firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password);
+			errHandler.catch((e) => console.log(e.message));
 	}
 	userLoginHandler(e) {
-		//do some different stuff
+		const errHandler = firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password);
+      errHandler.catch((e) => console.log(e.message));
 	}
 	loginChangeHandler(e) {
 		this.setState({
@@ -37,6 +41,27 @@ class App extends Component {
 	}
 	//END OF LOGIN PAGE COMPONENTS
 
+	componentWillMount() {
+		//FIREBASE SET UP
+		var config = {
+			apiKey: API.fireBaseApiKey,
+			authDomain: "sotm-66f9b.firebaseapp.com",
+			databaseURL: "https://sotm-66f9b.firebaseio.com",
+			projectId: "sotm-66f9b",
+			storageBucket: "sotm-66f9b.appspot.com",
+			messagingSenderId: "1016933819692"
+		};
+		firebase.initializeApp(config);
+		//END FIREBASE SET UP
+		//FIREBASE AUTH
+		firebase.auth().onAuthStateChanged((User) => {
+      if (User) {
+        console.log(User.email, 'logged in!');
+      } else {
+        console.log('Logged out!');
+      }
+    });
+	}
 	render() {
 	return (
 			<div>
