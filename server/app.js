7af3940430157d94bd54353fcf89f7fd7c8095e2
Serghei Madan <madan.serghei@yahoo.com>
2018-03-15 11:21:39 -0700

    structured a bit of files

--- server/app.js
+++ server/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const router = express.Router();
+const http = require('http').Server(app)
 const path = require('path');
+const io = require('socket.io')(http)
+
 
 const port = 9001;
 
@@ -10,14 +13,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/api', router);
 
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
 app.get('/', function(req, res) {
     console.log('Serving up Index.html');
-    res.sendFile(path.join(__dirname + '/../client/index.html'));
+    res.sendFile(path.join(__dirname + '/../client/dist'));
 });
 
-app.get('/bundle.js', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../client/bundle.js'));
+
+//Routes handlers
+// const authRoutes = require('./routes').auth;
+// const apiRoutes = require('./routes').api;
+// const addPageRoutes = require('./routes').pages;
+
+io.on('connection', function(socket) {
+    console.log('player connected');
+
+    socket.on('message', function (socket) {
+        console.log('Got the message form client: ' )
+    });
 });
 
-app.listen(port);
-console.log('We are listening on port', port);
+let server = app.listen(port, function() {
+    console.log('We are listening on port', port);
+});
