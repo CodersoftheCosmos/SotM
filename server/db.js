f81178ab189b4b551ef4fbcbf1d79a1171d97b7f
Jarrett Portnoy <jarrettrportnoy@gmail.com>
2018-03-15 15:27:07 -0700

    add schema and model for both characters and users

--- server/db.js
+++ server/db.js
@@ -8,4 +8,54 @@ db.once('once', function() {
     console.log('Database is connected and listening on Port 27017');
 });
 
-module.exports = {db};
+let characterSchema = mongoose.Schema({
+    name: String,
+    hp: Number,
+    power: String,
+    cardDeck: Array,
+    charType: String
+});
+
+let userSchema = mongoose.Schema({
+    username: String,
+    stats: {
+      wins: Number,
+      losses: Number,
+      favChar: String,
+      totDmgDone: Number
+    }
+});
+
+let Character = mongoose.model('Character', characterSchema);
+let User = mongoose.model('User', userSchema);
+
+let newCharacter = (name, hp, power, cardDeck, charType) => {
+  let character = new Character({
+      name: name, 
+      hp: hp, 
+      power: power, 
+      cardDeck: cardDeck, 
+      charType: charType
+    });
+  character.save((err, character) => {
+      if(err) {
+          return console.log("Something went wrong while saving. ", err);
+      }
+      console.log('Saved ', character);
+  })
+}
+
+let newUser = (username, stats = {wins: 0, losses: 0, favChar: 'Legacy', totDmgDone: 0}) => {
+    let user = new User({
+      username: username,
+      stats: stats
+    });
+    user.save((err, user) => {
+        if(err) {
+            console.log("Something went wrong while saving. ", err);
+        }
+        console.log('Saved ', user);
+    })
+}
+
+module.exports = {db, newCharacter, newUser};
