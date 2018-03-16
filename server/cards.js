2bc6406061210867dbeed6f6b5c87eb29e1c2cdd
Jarrett Portnoy <jarrettrportnoy@gmail.com>
2018-03-15 17:40:26 -0700

    add photo column to card schema, add rps card data, saved photos for each card

---
+++ server/cards.js
@@ -0,0 +1,5 @@
+let rpsCards = [
+  {name: 'Rock', photo: '../client/dist/photos/rock.png'},
+  {name: 'Paper', photo: '../client/dist/photos/paper.png'},
+  {name: 'Scissors', photo: '../client/dist/photos/scissors.png'}
+]
