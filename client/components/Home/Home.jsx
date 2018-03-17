b0d8e6f112fd84d144e52c99f9eb20fdf7d0a4c7
Jchen <jcdigital316@gmail.com>
2018-03-17 10:57:15 -0700

    fixed a merge issue

--- client/components/Home/Home.jsx
+++ client/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Stats from './Stats.jsx';
+
 class Home extends Component {
 	constructor(props){
 		super(props)
@@ -8,15 +9,9 @@ class Home extends Component {
 	render() {
 	return (
 			<div align="center">
-<<<<<<< HEAD
 				<button name="join" onClick={console.log('join')}>Join A Game</button>
 				<br />
 				<button name="create" onClick={console.log('create')}>Create A Game</button>
-=======
-				<button name="join" onClick={console.log()}>Join A Game</button>
-				<br />
-				<button name="create" onClick={console.log()}>Create A Game</button>
->>>>>>> b7b28afe9a9eb44f1f5bcddf3b118c28d770aeb6
 				<hr />
 				<Stats />
 			</div>
