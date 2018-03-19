7f16c89ba3b9e4b8e5aff0b81f542c28a9abd69c
Serghei Madan <madan.serghei@yahoo.com>
2018-03-19 14:18:39 -0700

    basic connection on dedicated socket server

--- socket-server/src/index.js
+++ socket-server/src/index.js
@@ -1,59 +1,55 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const router = express.Router();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const path = require('path');
+const io = require('socket.io')(server)
 
-const port =  9001
+const port = 9002;
 
-const socket = io.listen(9001);
-
-
+let players = [];
 let playedCount = 0;
 let game = {};
-let numPlayers = 0;
-let players = [];
 
-
-io.on('connection', function(socket) { 
+io.on('connection', function(socket) {
     console.log('player connected');
-    // numPlayers++
-    // console.log(numPlayers)
-    // players.push(socket)  
-    // //check how many players are connected , we need to wait until 2
-    // //push the player id into the array
-    // if (numPlayers === 1) {
-    //     socket.emit('handlePlayer', {player: 'playerOne'})
-    //     socket.emit('handleStatus', {msg:'waiting for another player'})
-    // } else if (numPlayers === 2) {
-    //     socket.emit('handlePlayer', {player: 'playerTwo'})
-    //     players.forEach(function(player) { player.emit('handleStatus', {msg:'get ready for the game'}) })
-    // } else {
-    //     socket.emit('handleFull', {msg:'lobby full, please wait..'})
-    // }
+//     if (players.length === 0){
+//         players.push(socket);
+//         socket.emit('handlePlayer', {msg:"playerOne"});
+//         socket.emit('status', {msg: 'waiting for another player..'})
+//     } else if (players.length === 1) {
+//         players.push(socket);
+//         socket.emit('handlePlayer', {msg: 'playerTwo'});
+//         players.forEach(player => {player.emit('status', {msg: 'Lets play'})})
+//     } else {
+//         socket.emit('handleWelcome', {msg: 'the server is full'})
+//     }
 
-    // socket.on('played', function(packet) {
-    //     if(socket === players[0]) {
-    //         game.player1 = packet.playerOneChoice;
-    //         playedCount++
-    //     } else if (socket === players[1]) {
-    //         game.player2 = packet.playerTwoChoice;
-    //         playedCount++
-    //     } else {
-    //         console.error('something happened')
-    //     }
-    //     if (playedCount === 2) {
-	// 		playedCount = 0;
-	// 		var champ = winner(game.player1, game.player2);
-	// 		players.forEach(player => {
-	// 			player.emit('gameOver', {winner:champ, player1:game.player1, player2:game.player2});
-	// 		});
-	// 	}
-    // });
+//     socket.on('played', function(packet) {
+//         if(scoket === players[0]) {
+//             game.player1 = packet.playerOneChoice;
+//             playedCount++
+//         } else if (socket === players[1]) {
+//             game.player2 = packet.playerTwoChoice;
+//             playedCount++
+//         } else {
+//             console.error('something happened')
+//         }
+//         if (playedCount === 3) {
+// 			playedCount = 0;
+// 			var champ = winner(game.player1, game.player2);
+// 			players.forEach(player => {
+// 				player.emit('gameOver', {winner:champ, player1:game.player1, player2:game.player2});
+// 			});
+// 		}
+//     });
 
     socket.once('disconnect', () => {
-        // players.splice(players.indexOf(socket), 1);
-        // numPlayers --;
-        // console.log(numPlayers)
+		// players.splice(players.indexOf(socket), 1);
+		// players.forEach(player => {
+		// 	player.emit('playerLeft', {msg: "The other player left"});
+		// });
 		socket.disconnect();
 		console.log('a user disconnected.');
     });
@@ -63,4 +59,5 @@ io.on('connection', function(socket) {
 server.listen(port, function() {
     console.log('We are listening on port', port);
 });
+
 
