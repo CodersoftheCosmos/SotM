3116d68b8318f25f7200d3a93dfed9c347ec30f2
Serghei Madan <madan.serghei@yahoo.com>
2018-03-27 18:31:16 -0700

    adding redis

--- server/controller/leaderboard.js
+++ server/controller/leaderboard.js
@@ -1,7 +1,7 @@
 const db = require('../db');
 const models = require('../models/index')
 const redis = require('redis')
-const config = require('../config')
+const config = require('../../config')
 
 const client = redis.createClient(config.redis_port, config.redis_host)
 
