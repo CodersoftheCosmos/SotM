d155b6d3f422bc163273e27f4068af2b6dc585a8
Jarrett Portnoy <jarrettrportnoy@gmail.com>
2018-04-03 19:33:20 -0700

    fix merge conflicts

--- client/components/gameView/Player1View.jsx
+++ client/components/gameView/Player1View.jsx
@@ -17,6 +17,14 @@ class PLayer1View extends Component {
         }
        
     }
+
+    handleHover(e) {
+        console.log('this is handle hover in player1')
+        if([...e.target.classList].includes('large')) {
+            e.target.classList.add('small');
+            e.target.classList.remove('large');
+        }
+    }
     
     
     render() {
@@ -31,7 +39,7 @@ class PLayer1View extends Component {
                         <img src={this.props.currentState.hero.imageUrl} className="charIcon" />
                         <span>
                             {this.props.currentState.hand.map((card, i) => {
-                                return (<img className="cards small" src={card.photo} key={i} onClick={(e)=>{ this.props.handleCard(card); this.enlargeCard(e)}} />)
+                                return (<img className="cards card small" src={card.photo} key={i} onClick={(e)=>{ this.props.handleCard(card); this.enlargeCard(e)}} onMouseOut={(e)=>{this.handleHover(e)}} />)
                             })}
                         </span>
                         <button onClick={() => {console.log(this.props.selectedCard)}} >props</button>
@@ -49,45 +57,73 @@ class PLayer1View extends Component {
                             background-color: yellow;
                             border: solid black 2px;
                         }
+
                         .health {
                             radius: 5px;
                             background-color: red;
                         }
+
                         p {
                             displaye: inline;
                         }
+
                         .heroInfo  {
                             display: inline;
                             padding-left: 20px;
                         }
+
                         .playerName {
                             display: inline;
                         }
+
                         .p1 {
                             border: solid 1px;
                         }
+
                         .deck {
                             float: right;
                             margin: 10px;
                         }
+
                         .cards {
                             border: solid 1px;
                             border-radius: 5px;
                             margin: 2px;
                         }
+
                         .charIcon {
                             height: 200px;
                             width: 200px;
                             border: solid 3px;
                             border-radius: 50%;
                         }
+
                         .small {
+                            transition: all 1s ease-in-out;
                             height: 100px;
                             width: 70px;
                         }
+
                         .large {
+                            transition: all 1s ease-in-out;
                             height: 300px;
                             width: 210px;
+                        }
+
+                        .card {
+                            transition: all 1s ease-in-out;
+                            background: #fff;
+                            display: inline-block;
+                            margin: 2px;
+                            box-shadow: 10px 10px 5px rgba(0, 0, 0, 0.5);
+                            z-index: 5;
+                        }
+
+                        .card:hover {
+                            transition: all 1s ease-in-out;
+                            transform: scale(2);
+                            box-shadow: 30px 30px 15px rgba(0, 0, 0, 0.2);
+                            z-index: 10;
                         }
                     `}
                 </style>
@@ -97,3 +133,4 @@ class PLayer1View extends Component {
     
 }
 export default PLayer1View;
+
