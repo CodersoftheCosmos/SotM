3ecd82ddcc61148f8e47d95655c3b7a757f022c0
Jarrett Portnoy <jarrettrportnoy@gmail.com>
2018-04-06 14:31:53 -0700

    inplay cards now enlarge on hover

--- client/components/gameView/Player2View.jsx
+++ client/components/gameView/Player2View.jsx
@@ -52,7 +52,7 @@ class PLayer2View extends Component {
                     <div className="power">Power: {this.props.currentState.hero.power}</div>
                     <span>
                         {this.props.inplay.map((card, i) => {
-                            return (<img className="cards small" src={card.photo} key={i} />)
+                            return (<img className="cards card small" src={card.photo} key={i} />)
                         })}
                     </span>   
                     <style>
